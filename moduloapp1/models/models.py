4f7d7c157c5f79d50b629c0c4985086256d5ce4c
Jose David <josedavid@MacBook-Pro-de-mac1.local>
2021-07-08 08:13:22 -0600

    Commit

--- moduloapp1/models/models.py
+++ moduloapp1/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class ../user/moduloapp1(models.Model):
-#     _name = '../user/moduloapp1.../user/moduloapp1'
-#     _description = '../user/moduloapp1.../user/moduloapp1'
+class ../user/moduloapp1(models.Model):
+     _name = '../user/moduloapp1.../user/moduloapp1'
+     _description = '../user/moduloapp1.../user/moduloapp1'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
