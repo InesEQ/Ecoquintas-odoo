4eb6d7241fad3bfdb64b2822472eba22301f7748
Jose David <jsolis@ecoquintas.com>
2022-05-18 02:12:32 +0000

    test modulo custom

--- desarrollo1/models/.ipynb_checkpoints/models-checkpoint.py
+++ desarrollo1/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
- from odoo import models, fields, api
+from odoo import models, fields, api
 
 
- class desarrollo1(models.Model):
-     _name = 'desarrollo1.desarrollo1'
-     _description = 'desarrollo1.desarrollo1'
+class desarrollo1(models.Model):
+    _name = 'desarrollo1.desarrollo1'
+    _description = 'desarrollo1.desarrollo1'
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
-     @api.depends('value')
-     def _value_pc(self):
-         for record in self:
-             record.value2 = float(record.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
