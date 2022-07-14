5935d3e9da0df84de65807bf0781458e0dbf9c63
EcoQuintas <informatica@ecoquintas.com>
2022-07-14 19:10:19 +0000

    prueba apppagos 12

--- apppagos/models/utilities.py
+++ apppagos/models/utilities.py
@@ -1,7 +1,9 @@
 #from odoo import models, fields, api
 import requests
 
-class utilities():
+class utilities(models.Model):
+    _name = 'apppagos.apppagos'
+    _description = 'apppagos.apppagos'
     
     def request_api():
         x = requests.get('http://181.193.143.38:5050/')
