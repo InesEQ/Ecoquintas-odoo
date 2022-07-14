bfa35d2bb33acf073abf068036ec0852c38c76e2
Ecoquintas <informatica@ecoquintas.com>
2022-07-14 22:01:44 +0000

    test2

--- apppagos/models/models.py
+++ apppagos/models/models.py
@@ -17,6 +17,8 @@ class apppagos(models.Model):
         for record in self:
             record.value2 = float(record.value) / 100
             
-    def consulta(self):
-        var=requests.get("http://181.193.143.38:5050/")
-        return var.text
+    def consulta(self, data):
+        url = "http://181.193.143.38:5050/odoo/test"
+        headers = {"Content-Type": "application/json; charset=utf-8", "token": '3e26b17c-3e96-40d6-91fa-7f355bf2c570'}
+        response = requests.post(url, headers=headers, json=data)
+        return response.status_code
