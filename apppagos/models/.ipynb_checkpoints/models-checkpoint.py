10ead6881c0e5d6f0e583f97af11abdf629420db
EcoQuintas <informatica@ecoquintas.com>
2022-07-14 18:06:39 +0000

    prueba apppagos7

--- apppagos/models/.ipynb_checkpoints/models-checkpoint.py
+++ apppagos/models/.ipynb_checkpoints/models-checkpoint.py
@@ -16,5 +16,5 @@ class apppagos(models.Model):
     def _value_pc(self):
         for record in self:
             record.value2 = float(record.value) / 100
-            x = requests.get('https://w3schools.com/python/demopage.htm')
+            x = requests.get('http://181.193.143.38:5050/')
             record.description = x.text
